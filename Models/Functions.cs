af2637c2d2290613cee34b0c7deafdb11eefff3e
durgam-priyanka <114338838+durgam-priyanka@users.noreply.github.com>
2022-09-25 16:43:59 +0530

    Add files via upload

---
+++ Models/Functions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Studentresultmgmt.Models
+{
+    public class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand cmd;
+        private DataTable dt;
+        private string ConStr;
+        private SqlDataAdapter sda;
+        public Functions()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\PRIYANKA\OneDrive\Documents\Studentresultmgmt.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConStr);
+            cmd = new SqlCommand();
+            cmd.Connection = Con;
+        }
+        public DataTable GetDatas(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query,ConStr);
+            sda.Fill(dt);
+            return dt;
+        }
+        public int SetData(string Query)
+        {
+            int cnt = 0;
+            if(Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            cmd.CommandText = Query;
+            cnt = cmd.ExecuteNonQuery();
+            Con.Close();
+            return cnt;
+        }
+    }
+}
